e1937c1f1452d5634e047a65035a0630b26c64d4
fairuz <sakhiyfairuz@gmail.com>
2024-12-31 20:43:37 +0700

    imported old home layout (temp)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="font-anek-devanagari">
+	<div
+		class="flex h-full min-h-screen flex-col justify-between bg-gradient-to-tr from-zinc-400 via-neutral-300 to-gray-100 px-5 py-5 lg:px-10"
+	>
+		<div class="flex flex-wrap items-end"></div>
+
+		<div class="flex flex-col justify-end gap-5">
+			<h1>
+				<div class="text-3xl">Hello!, my name is</div>
+				<div class="font-dm-serif text-6xl">Fairuz Sakhiy Hibban Adijatmiko</div>
+			</h1>
+
+			<div>
+				<p class="text-3xl">I'm a software developer based on Indonesia</p>
+			</div>
+			<div class="flex flex-col items-center justify-center">
+				<p class="text-xl font-bold">Scroll down to see more (coming soon)</p>
+				<!-- <ArrowDownwardIcon /> -->
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.main {
+		font-family: 'Inter', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 400;
+		font-style: normal;
+	}
+</style>
